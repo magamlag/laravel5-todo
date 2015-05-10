81d8596c3e4e4c8747c159685f6adae9bbf86946
magamlag <lagovskiy@gmail.com>
2015-05-10 17:35:20 -0400

    Modified Project Index/Show Pages

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,4 +1,5 @@
-@extends('...app')
+<!-- /resources/views/projects/index.blade.php -->
+@extends('app')
  
 @section('content')
     <h2>Projects</h2>
@@ -8,8 +9,19 @@
     @else
         <ul>
             @foreach( $projects as $project )
-                <li><a href="{{ route('projects.show', $project->slug) }}">{{ $project->name }}</a></li>
+                <li>
+                    {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('projects.destroy', $project->slug))) !!}
+                        <a href="{{ route('projects.show', $project->slug) }}">{{ $project->name }}</a>
+                        (
+                            {!! link_to_route('projects.edit', 'Edit', array($project->slug), array('class' => 'btn btn-info')) !!},
+                            {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
+                        )
+                    {!! Form::close() !!}
+                </li>
             @endforeach
         </ul>
     @endif
+    <p>
+        {!! link_to_route('projects.create', 'Create Project') !!}
+    </p>
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,3 +1,4 @@
+<!-- /resources/views/projects/show.blade.php -->
 @extends('app')
  
 @section('content')
@@ -8,8 +9,22 @@
     @else
         <ul>
             @foreach( $project->tasks as $task )
-                <li><a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a></li>
+                <li>
+                    {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('projects.tasks.destroy', $project->slug, $task->slug))) !!}
+                        <a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a>
+                        (
+                            {!! link_to_route('projects.tasks.edit', 'Edit', array($project->slug, $task->slug), array('class' => 'btn btn-info')) !!},
+ 
+                            {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
+                        )
+                    {!! Form::close() !!}
+                </li>
             @endforeach
         </ul>
     @endif
+ 
+    <p>
+        {!! link_to_route('projects.index', 'Back to Projects') !!} |
+        {!! link_to_route('projects.tasks.create', 'Create Task', $project->slug) !!}
+    </p>
 @endsection
